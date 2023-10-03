c3cc15374822c5e06274834c12116e5f85091dcf
Alexander Trembiński <alexandertrembinski05@gmail.com>
2023-10-03 09:48:37 +0200

    Table with data from AI

--- script.js
+++ script.js
@@ -16,14 +16,70 @@ async function showLink() {
     document.getElementById('linK').innerHTML = url;
 }
 
-async function showData() {
-    let nrPage = document.getElementById("pageNumber").value;
-    let pageSize = document.getElementById("pageSize").value;
-    let url = `https://api.kt-24.pro/api/logs/sort?page=${nrPage}&pageSize=${pageSize}`;
+let currentPage = 0;
+const pageSize = 12;
+
+async function showData(page) {
+    let url = `https://api.kt-24.pro/api/logs/sort?page=${page}&pageSize=${pageSize}`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
-    document.getElementById('result').innerHTML = data.logs[0].account;
+    console.log(url)
+    
+    const resultElement = document.getElementById('result');
+    resultElement.innerHTML = '';
+
+    const table = document.createElement('table');
+    const td = ['account', 'accountCountry', 'baselinkerStatus', 'createdAt', 'deliveryCountry', 'deliveryMethod', 'id', 'invoiceCountry', 'isStock', 'isValid', 'marketplace', 'nip', 'orderId', 'paymentMethod', 'viesUid', 'warehouse'];
+
+    const tr = document.createElement('tr');
+    td.forEach(thText => {
+        const th = document.createElement('th');
+        th.textContent = thText;
+        tr.appendChild(th);
+    });
+    table.appendChild(tr);
+
+    data.logs.forEach((el) => {
+        const row = document.createElement('tr');
+        td.forEach(th => {
+            const text = document.createElement('td');
+            text.textContent = el[th];
+            row.appendChild(text);
+        });
+        table.appendChild(row);
+    });
+
+    resultElement.appendChild(table);
+}
+function getFirstPage() {
+    currentPage = 0;
+    showData(currentPage);
+}
+
+function getNextPage() {
+    currentPage++;
+    showData(currentPage);
+}
+
+function getPreviousPage() {
+    if (currentPage > 0) {
+        currentPage--;
+        showData(currentPage);
+    }
+}
+
+showData(currentPage);
+
+
+
+
+
+
+
+
+
+
+
 
 // const grid = new Grid({
 //     columns: ['account', 'accountCountry', 'baselinkerStatus', 'createdAt', 'deliveryCountry', 'deliveryMethod', 'id', 'invoiceCountry', 'isStock', 'isValid', 'marketplace', 'nip', 'orderId', 'paymentMethod', 'viesUid', 'warehouse'],
@@ -38,29 +94,3 @@ async function showData() {
 //     document.getElementById("result").innerHTML =+ '<table><tr><th>account</th><th>accountCountry</th><th>baselinkerStatus</th><th>createdAt</th><th>deliveryCountry</th><th>deliveryMethod</th><th>id</th><th>invoiceCountry</th><th>isStock</th><th>isValid</th><th>marketplace</th><th>nip</th><th>orderId</th><th>paymentMethod</th><th>viesUid</th><th>warehouse</th></tr>'
 //     + '<tr><td>' + el.account + '</td><td>' + el.accountCountry + '</td><td>' + el.baselinkerStatus +'</td><td>' + el.createdAt + '</td><td>' + el.deliveryCountry + '</td><td>' + el.deliveryMethod + '</td><td>' + el.id + '</td><td>' + el.invoiceCountry + '</td><td>' + el.isStock + '</td><td>' + el.isValid + '</td><td>' + el.marketplace + '</td><td>' + el.nip + '</td><td>' + el.orderId + '</td><td>' + el.paymentMethod + '</td><td>' + el.viesUid + '</td><td>'+ el.warehouse + '</td></table>'
 // })
-
-const table = document.createElement('table');
-    const tr = document.createElement('tr');
-    const td = ['account', 'accountCountry', 'baselinkerStatus', 'createdAt', 'deliveryCountry', 'deliveryMethod', 'id', 'invoiceCountry', 'isStock', 'isValid', 'marketplace', 'nip', 'orderId', 'paymentMethod', 'viesUid', 'warehouse'];
-
-    td.forEach(thText => {
-        const th = document.createElement('th');
-        th.textContent = thText;
-        tr.appendChild(th);
-    });
-    table.appendChild(tr);
-
-    data.logs.forEach((el) => {
-        const row = document.createElement('tr');
-        td.forEach(th => {
-            const cell = document.createElement('td');
-            cell.textContent = el[th];
-            row.appendChild(cell);
-        });
-        table.appendChild(row);
-    });
-
-    const resultElement = document.getElementById('result');
-    resultElement.innerHTML = '';
-    resultElement.appendChild(table);
-}
